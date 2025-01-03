ef52fa91302fbffa07bf10e98b56613035607abc
Ubuntu <ubuntu@ip-172-31-45-69.eu-west-3.compute.internal>
2025-01-03 14:40:33 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+function App() {  
+  return (  
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>  
+      <h1>Hello, World!</h1>  
+      <p>Welcome to your first React application on EC2!</p>  
+    </div>  
+  );  
+}  
+
+export default App;
