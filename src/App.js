65f8334641bfd3fd0dc9456f0debbddd4aca81db
usama0987 <121391282+usama0987@users.noreply.github.com>
2025-01-10 20:09:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {  
   return (  
     <div style={{ textAlign: 'center', marginTop: '50px' }}>  
-      <h1>Hello, World!</h1>  
+      <h1>Hello, Client</h1>  
       <p>Welcome to your first React application on EC2!</p>  
     </div>  
   );  
